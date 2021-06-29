0c84200754e5816f9e650ac089b140c0ad1ff0c1
Akshay-VK <akshaykaruvathil@gmail.com>
2021-06-29 09:32:23 +0530

    successful in trying to take input from user..YAYnode index.js

--- index.js
+++ index.js
@@ -9,84 +9,106 @@ client.login(process.env.BOTTOKEN);
 
 console.log('BOT LOGGED IN!!...');
 
-client.on("ready",botReady);
+client.on("ready", botReady);
 
-function botReady(){
+function botReady() {
 	console.log("bot ready...");
 }
 
-var mobs = ["CREEPER","SKELETON","ZOMBIE","ENDERMAN", "ENDER-DRAGON","SPIDER","WITCH","VILLAGER","PARROT","DOG","CAT","SHULKER","WITHER","PIGLIN"];
+var mobs = ["CREEPER", "SKELETON", "ZOMBIE", "ENDERMAN", "ENDER-DRAGON", "SPIDER", "WITCH", "VILLAGER", "PARROT", "DOG", "CAT", "SHULKER", "WITHER", "PIGLIN", "PILLAGER", "HORSE", "PANDA"];
 
-var biomes = ["plains","swamps","forest","savanna","taiga","snowy tundra","mesa","jungle"];
+var biomes = ["plains", "swamps", "forest", "savanna", "taiga", "snowy tundra", "mesa", "jungle"];
 
-var people ={};
+var people = {};
 
-client.on("message",gotMessage);
+client.on("message", gotMessage);
 
-function pickRandom(arr){
-	return arr[Math.floor(Math.random()*arr.length)];
+function pickRandom(arr) {
+	return arr[Math.floor(Math.random() * arr.length)];
 }
 
-function gotMessage(msg){
+function gotMessage(msg) {
 	console.log(msg.content);
-	
+
 	var words = msg.content.split(' ');
 
-	if(msg.channel.id === '858225245060333569'){
-	  if(msg.content === '*help'){
-	    msg.channel.send('All commands must be prefixes with a star(*)\n\n**rnmob**-\tDisplays a random mob name\n**nick**-\tDisplays the set nickname\n**setnick**-\tSets a nickname\n**startgame**-\tStarts a new game\n**stopgame**-\tStops the current game\n**creators**-\tDisplays the creators of this bot');
-	  }
-	  //RANDOM MOB
-		if(msg.content === '*rnmob'){
-			msg.reply("Your random mob is: "+mobs[Math.floor(Math.random()*mobs.length)]);
-		}
-		//CREATORS
-		if(msg.content === '*creators'){
+	if (msg.channel.id === '858225245060333569') {
+
+
+		if (msg.content === '*help') {
+
+			msg.channel.send('All commands must be prefixes with a star(*)\n\n**rnmob**-\tDisplays a random mob name\n**nick**-\tDisplays the set nickname\n**setnick**-\tSets a nickname\n**startgame**-\tStarts a new game\n**stopgame**-\tStops the current game\n**creators**-\tDisplays the creators of this bot');
+
+		} else
+		if (msg.content === '*rnmob') { //RANDOM MOB
+			msg.reply("Your random mob is: " + mobs[Math.floor(Math.random() * mobs.length)]);
+
+		} else
+		if (msg.content === '*creators') { //CREATORS
 			msg.reply("MC Quest was made by 2 simple people who led simple lives. They went by the name of Akshay and Kishan.");
-		}
-		//SET NICKNAME
-		if(words[0] === '*setnick'){
-		  	people[msg.author.id] = {'nickname':words[1]};
-			msg.reply('Your nickname is set to '+words[1]);
-		  	console.log(people);
-		}
-		//GET NICKNAME
-		if(msg.content === '*nick'){
-		  	if(people.hasOwnProperty(msg.author.id) && people[msg.author.id].hasOwnProperty('nickname')){
-			    	msg.reply("Your nickname is "+people[msg.author.id].nickname);
-		  	}else{
-		    		msg.reply(`You haven't set a nickname yet. To do so, type *setnick (your nickname)`);
-		  	}
-		}
-		//START A GAME
-		if(msg.content === '*startgame'){
-			if(!people.hasOwnProperty(msg.author.id)){
+		} else
+		if (words[0] === '*setnick') { //SET NICKNAME
+			people[msg.author.id] = {
+				'nickname': words[1]
+			};
+			msg.reply('Your nickname is set to ' + words[1]);
+			console.log(people);
+		} else
+		if (msg.content === '*nick') { //GET NICKNAME
+			if (people.hasOwnProperty(msg.author.id) && people[msg.author.id].hasOwnProperty('nickname')) {
+				msg.reply("Your nickname is " + people[msg.author.id].nickname);
+			} else {
+				msg.reply(`You haven't set a nickname yet. To do so, type *setnick (your nickname)`);
+			}
+		} else
+		if (msg.content === '*startgame') { //START GAME
+			if (!people.hasOwnProperty(msg.author.id)) {
 				people[msg.author.id] = {};
 			}
 			handleGame(msg);
-		}
-		//STOP A GAME
-		if(msg.content === "*stopgame"){
+		} else
+		if (msg.content === "*stopgame") { //STOP GAME
 			var id = msg.author.id;
-			if(people[id]['gameState'] == true){
+			if (people[id]['gameState'] == true) {
 				people[id]['gameState'] = false;
 				msg.reply("Game stopped.");
-			}else{
+			} else {
 				msg.reply(`You don't have any game running right now...`);
 			}
+		} else
+		if (msg.content === "*inputtest") {
+			msg.channel.send("Type a name.");
+
+			// `m` is a message object that will be passed through the filter function
+			function filter(m) {
+				return m.author.id == msg.author.id;
+			}
+			const collector = msg.channel.createMessageCollector(filter, {
+				time: 15000,
+				max: 1
+			});
+
+			collector.on('collect', m => {
+				console.log(`Collected ${m.content}`);
+				msg.channel.send("Hello "+m.content);
+			});
+
+			collector.on('end', collected => {
+				console.log(`Collected ${collected.size} items`);
+			});
 		}
 	}
 }
 
-function handleGame(msg){
+function handleGame(msg) {
 	var id = msg.author.id;
 	console.log(people);
-	if(people[id]['gameState'] == true){
+	if (people[id]['gameState'] == true) {
 		msg.reply("Umm..you have a game running. If you want to start a new game, type *stopgame and then start a new game");
-	}else{
+	} else {
 		people[id]['gameState'] = true;
 		var biome = pickRandom(biomes);
 		people[id]['currentBiome'] = biome;
-		msg.reply("New game started....\nYou spawned in the "+biome+" biome!");
+		msg.reply("New game started....\nYou spawned in the " + biome + " biome!");
 	}
 }
